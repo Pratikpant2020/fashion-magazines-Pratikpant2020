d1575ffab14b3bad20b98f3fa5caa6ee6fc8a08a
bhatt <bhatt.meena@yahoo.com>
2024-10-13 11:06:44 -0400

    fashion magzine

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,10 @@
--- Add your sql here
+select
+    customer_name AS Customer
+    SUM(price_per_month) AS Amount Due
+From
+    subscriptions
+WHERE
+   subscription_type = 'Fashion Magazine'  AND 
+   order_status = 'unpaid'
+GROUP BY 
+    customer_name;
